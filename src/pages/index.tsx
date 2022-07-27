323065628dfed7f11fdb08449c4562ebc61c4114
Luis Cetta <filipe.cetta@gmail.com>
2022-07-27 00:43:07 -0300

    consuming Stripe API products price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss'
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome </span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month.</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LQ0aSIh5HDjPJuIahn0xOab', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+
+    // Dica: sempre salve valores monetários em centavos no BD - dividindo por 100
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
