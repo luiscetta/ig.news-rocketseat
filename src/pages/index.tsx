3c3b4fad8618ee6f0c1ab774f475f1459bab3b37
Luis Cetta <filipe.cetta@gmail.com>
2022-07-26 23:41:49 -0300

    creating header and button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import styles from '../styles/home.module.scss';
-
 import Head from 'next/head';
 
 export default function Home() {
@@ -8,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello World!
       </h1>
     </>
